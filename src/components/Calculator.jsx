cc9b6a8e609a2758379db451497a89968e319940
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-01-03 16:44:03 +0200

    Update Calculator.jsx

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -3,7 +3,7 @@ import Button from "./Button";
 import ScreenDisplay from "./ScreenDisplay";
 
 const Calculator = ({ btns, handler, result }) => {
-  //render the btns array by using the Button component
+  //render each button in the btns array by using the Button component
   const renderedButtons = btns.map((btn, index) => {
     return (
       <Button
