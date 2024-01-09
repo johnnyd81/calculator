cb54d0c5d6c89d0ff5c1953d2007e63a21da35d0
Jonathan Johnson <95863021+johnnyd81@users.noreply.github.com>
2024-01-09 17:26:40 +0200

    Update ScreenDisplay.jsx

--- src/components/ScreenDisplay.jsx
+++ src/components/ScreenDisplay.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const ScreenDisplay = ({ result }) => {
+  //displays the result of the calculation to the calculator screen
   return (
     <div className="display">
       <span className="digitValue">{result}</span>
